5e8791bd63ba4f2e9020e4c244a30e4524e33a87
Arturas Budreika <budreika.arte@gmail.com>
2020-04-25 08:22:18 +0200

    Updated app.py
    - code properly indented under dunder main so that code would not run if imported
    - moved function's docstrings bellow the function declaration

--- app.py
+++ app.py
@@ -8,15 +8,13 @@ my_teams = copy.deepcopy(constants.TEAMS)
 my_players = copy.deepcopy(constants.PLAYERS)
 
 
-"""
-Clean up data:
-clear_data() function cleans data from the original constants.py
-data collection. Experience converted to boolean: True if YES or
-False if NO. Height converted to an integer.
-"""
-
-
 def clear_data():
+    """
+    Clean up data:
+    clear_data() function cleans data from the original constants.py
+    data collection. Experience converted to boolean: True if YES or
+    False if NO. Height converted to an integer.
+    """
     for data in my_players:
         data['height'] = int(data['height'][:2])
         data['guardians'] = data['guardians'].split(' and ')
@@ -28,43 +26,39 @@ def clear_data():
     return my_players
 
 
-"""
-Team balancing:
-player_balance() function assigns players to each team so that all three teams
-are evenly balanced by the total number of players.
-
-Using Methods: randrange() + pop() from
-https://www.geeksforgeeks.org/python-remove-random-element-from-list/
-"""
 team_pan = []
 team_war = []
 team_ban = []
 
 
 def player_balance(team):
+    """
+    Team balancing:
+    player_balance() function assigns players to each team so that all three
+    teams are evenly balanced by the total number of players.
+
+    Using Methods: randrange() + pop() from
+    https://www.geeksforgeeks.org/python-remove-random-element-from-list/
+    """
     while len(my_players) != 0 and len(team) < 6:
         team.append((my_players.pop(random.randrange(len(my_players)))))
     return team
 
 
-"""
-Display teams:
-teams_list() function displays teams in a list.
-"""
-
-
 def teams_list():
+    """
+    Display teams:
+    teams_list() function displays teams in a list.
+    """
     for index, item in enumerate(my_teams, 1):
         print(f'     {index}. {item}')
 
 
-"""
-Display players list:
-players_list() function displays selected team players in a list.
-"""
-
-
 def players_list(selected_team):
+    """
+    Display players list:
+    players_list() function displays selected team players in a list.
+    """
     selected_player_list = []
     for player in selected_team:
         if player['name']:
@@ -73,23 +67,20 @@ def players_list(selected_team):
     return ", ".join(selected_player_list)
 
 
-"""
-Display the stats:
-team_stats() displaying selected team stats
-"""
-
-
 def team_stats(team_position_in_list, balanced_team):
+    """
+    Display the stats:
+    team_stats() displaying selected team stats
+    """
     print(f'\n Team: {my_teams[team_position_in_list]} Stats:')
     print(' ---------------------')
     print(f' Total players: {len(balanced_team)}')
     print('\n Player on Team:')
     print(f' {players_list(balanced_team)}')
 
-# tool_name() function prints out tool name
-
 
 def tool_name():
+    # tool_name() function prints out tool name
     print("""
 --------------------------
 BASKETBALL TEAM STATS TOOL
@@ -98,10 +89,8 @@ BASKETBALL TEAM STATS TOOL
 """)
 
 
-# menu() function displays menu
-
-
 def menu():
+    # menu() function displays menu
     print(' Here are your options:')
     print('     1. Show Teams')
     print('     2. Quit')
@@ -113,58 +102,58 @@ if __name__ == '__main__':
     tool_name()
 
     # calling menu() function to load menu to the user
-menu()
+    menu()
 
-# calling below functions to clean data and assig players to each team
-clear_data()
-player_balance(team_pan)
-player_balance(team_war)
-player_balance(team_ban)
+    # calling below functions to clean data and assig players to each team
+    clear_data()
+    player_balance(team_pan)
+    player_balance(team_war)
+    player_balance(team_ban)
 
-# starting loop to evaluate user selection from menu
-while True:
-    try:
-        # prompting user to select an option from menu
-        user_selection = int(
-            input("\n Please select between option 1 or 2 and press ENTER to continue: "))
-    except ValueError:
-        print(" Oh no! That's not a valid value. Try again...")
-    else:
-        # option 2 quits tool
-        if user_selection == 2:
-            print('\n Quiting....')
-            break
-        #  option 1 loads team list
-        if user_selection == 1:
-            while True:
-                # teams_list() loading team list
-                teams_list()
-                try:
-                    # prompting the user to pick a team and handling errors if any
-                    user_team_selection = int(
-                        input("\n Please select a team and enter team number and press ENTER to continue: "))
-                except ValueError:
-                    print(" Oh no! Please choose between 1, 2 or 3. Try again...")
-                    continue
-                else:
-                    if user_team_selection > 3 or user_team_selection < 1:
+    # starting loop to evaluate user selection from menu
+    while True:
+        try:
+            # prompting user to select an option from menu
+            user_selection = int(
+                input("\n Please select between option 1 or 2 and press ENTER to continue: "))
+        except ValueError:
+            print(" Oh no! That's not a valid value. Try again...")
+        else:
+            # option 2 quits tool
+            if user_selection == 2:
+                print('\n Quiting....')
+                break
+            #  option 1 loads team list
+            if user_selection == 1:
+                while True:
+                    # teams_list() loading team list
+                    teams_list()
+                    try:
+                        # prompting the user to pick a team and handling errors if any
+                        user_team_selection = int(
+                            input("\n Please select a team and enter team number and press ENTER to continue: "))
+                    except ValueError:
                         print(" Oh no! Please choose between 1, 2 or 3. Try again...")
                         continue
-                    # displaying selected team stats
-                    elif user_team_selection == 1:
-                        team_position_in_list = 0
-                        balanced_team = team_pan
-                        team_stats(team_position_in_list, balanced_team)
-                    elif user_team_selection == 2:
-                        team_position_in_list = 1
-                        balanced_team = team_war
-                        team_stats(team_position_in_list, balanced_team)
-                    elif user_team_selection == 3:
-                        team_position_in_list = 2
-                        balanced_team = team_ban
-                        team_stats(team_position_in_list, balanced_team)
-                    break
-    # after team stats are displayed, prompting user to continue
-    input('\n Press ENTER to continue...')
-    print('')
-    menu()
+                    else:
+                        if user_team_selection > 3 or user_team_selection < 1:
+                            print(" Oh no! Please choose between 1, 2 or 3. Try again...")
+                            continue
+                        # displaying selected team stats
+                        elif user_team_selection == 1:
+                            team_position_in_list = 0
+                            balanced_team = team_pan
+                            team_stats(team_position_in_list, balanced_team)
+                        elif user_team_selection == 2:
+                            team_position_in_list = 1
+                            balanced_team = team_war
+                            team_stats(team_position_in_list, balanced_team)
+                        elif user_team_selection == 3:
+                            team_position_in_list = 2
+                            balanced_team = team_ban
+                            team_stats(team_position_in_list, balanced_team)
+                        break
+        # after team stats are displayed, prompting user to continue
+        input('\n Press ENTER to continue...')
+        print('')
+        menu()
